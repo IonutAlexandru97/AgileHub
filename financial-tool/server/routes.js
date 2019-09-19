671d9b9cbb4c41a9795d72cbb3c9a6535a12d371
IonutAlexandru97 <aleq67@yahoo.ro>
2019-09-19 22:37:46 +0300

    moved readme file

--- financial-tool/server/routes.js
+++ financial-tool/server/routes.js
@@ -6,12 +6,14 @@ const passport = require('./config/passport');
 
 // Users controller
 router.post('/register', usersController.register);
-router.post('/login', passport.authenticate("local"), function(req, res) {
+router.post('/login', passport.authenticate("local"), function(req, res, info) {
     if(req.user.username !== null) {
         res.status(200).json({
             statusText: 200,
             message: 'User ' + req.user.username + ' logged in!'
         });
+    }else{
+        console.log(info);
     }
 });
 
